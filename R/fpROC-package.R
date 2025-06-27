e97f1312e7c0061fce22d07e24ee2fa427d9683d
luismurao <luismurao@gmail.com>
2025-06-27 13:18:35 -0600

    Initial commit

---
+++ R/fpROC-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp evalCpp
+#' @importFrom RcppParallel RcppParallelLibs
+#' @useDynLib fpROC
+## usethis namespace: end
+NULL
---
+++ README.md
@@ -0,0 +1,2 @@
+# fpROC
+ The package features optimized C++ code for computing partial ROC. Parallelization using OpenMP (OMP) was achieved with assistance from the DeepSeek AI Assistant (https://deepseek.com).
